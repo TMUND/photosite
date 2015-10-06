c8fdcd0f1a31219a90d0c8ebc2575d500b340c08
Toni Munder <tonimunder@hotmail.com>
2015-10-06 14:50:44 -0700

    Some cleanup

--- js/main.js
+++ js/main.js
@@ -7,21 +7,6 @@
         $('.gallery').on('mouseover', function() {
 
         });
-
-      // Hovers and Clicks Practice
-      // ----------
-        $('.hoverbutton').on('mouseover', function() {
-          var panelId = $(this).attr('data-panelid');
-          $('#' + panelId).fadeToggle(500);
-        });
-
-        var content = "And I've changed!";
-
-        $('.clickbutton').on('click', function() {
-          var panelId = $(this).attr('data-panelid');
-          $('#' + panelId).toggle();
-          $('#' + panelId + ' .panel-body').html(content);
-        });
     }
   };
 
