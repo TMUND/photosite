61b0c9c2a9f48efac0808ba04b07d162f8dbba01
Toni Munder <tonimunder@hotmail.com>
2015-10-12 18:47:07 -0700

    Lots of changes, hover over gallery now works!

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Photography website sandbox
 
 ## TODO
 
-* Create banner with working buttons (make it responsive)
+* Create banner -- make it responsive
 * Write JS for banner to make it work
 * Create "front" page with galleries
 * Hook up gallery links so they leade to actual gallery page
--- js/main.js
+++ js/main.js
@@ -3,16 +3,6 @@
   window.App = {
     // ----------
     init: function() {
-
-        galleryTitles = ['Afghanistan', 'Pakistan', 'Russia', 'India'];
-
-        $('.flex-item').mouseenter(function() {
-            $(this).append('<div class="gallery-title">' + galleryTitles[0] + '</div>');
-        });
-
-        $('.flex-item').mouseleave(function() {
-            $(this).children('div').remove();
-        });
     }
   };
 
